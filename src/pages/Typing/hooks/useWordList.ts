5ce237924945fd3d7e0dfec48fa3961e62330ec7
meinezhang <meinezhang@tencent.com>
2024-04-07 11:33:16 +0800

    feat: #0 只有一个章节 & 随机排序

--- src/pages/Typing/hooks/useWordList.ts
+++ src/pages/Typing/hooks/useWordList.ts
@@ -12,6 +12,14 @@ export type UseWordListResult = {
   error: Error | undefined
 }
 
+function shuffleArray(array: any) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
+}
+
 /**
  * Use word lists from the current selected dictionary.
  */
@@ -35,7 +43,7 @@ export function useWordList(): UseWordListResult {
     } else if (isReviewMode) {
       newWords = reviewRecord?.words ?? []
     } else if (wordList) {
-      newWords = wordList.slice(currentChapter * CHAPTER_LENGTH, (currentChapter + 1) * CHAPTER_LENGTH)
+      newWords = shuffleArray(wordList)?.slice(currentChapter * CHAPTER_LENGTH, (currentChapter + 1) * CHAPTER_LENGTH)
     } else {
       newWords = []
     }
